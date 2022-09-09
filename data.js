db9da8d396d3559081a908e96fdeb4006dfa1ae4
ilham akbar <mohdilhamakbar10.com>
2022-09-09 16:34:35 +0700

    commit pertama

---
+++ data.js
@@ -0,0 +1,35 @@
+const nama = "ilham"
+let usia = 30;
+
+const biodata = document.getElementById('biodata');
+console.log(biodata);
+
+function generateBiodata() {
+  let generasi;
+
+    if (usia > 10 && usia < 18) {      
+      generasi = "generasi remaja";
+    } 
+    else if (usia > 18 && usia < 30) {
+      generasi = "generasi dewasa";
+    } 
+    else if (usia >= 30) {
+      generasi = "generasi tua";
+    }
+    else if (usia <10 && usia > 2) {
+      generasi = "generasi anak-anak"
+
+    }
+    else {
+      generasi = "generasi bayi"
+    }
+    return console.log('generasi saya adalah' , generasi);
+
+
+
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
